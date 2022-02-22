51e0252b67a8597da53ceb01b32764872976d1c7
mystickoder <89115482+mystickoder@users.noreply.github.com>
2022-02-22 12:36:43 -0500

    Add files via upload

---
+++ bettys_place.py
@@ -0,0 +1,31 @@
+print("Welome to Betty's, please place your order.")
+breakfast = ['pancakes', 'waffles', 'cereal', 'toast']
+sides= ['home fries', 'grits', 'eggs']
+drinks = ['water', 'cofee', 'tea']
+
+if "pancakes" in breakfast:
+    print("add eggs")
+    
+
+if "home fries" in sides:
+    print("add tea")
+    
+if "waffles" in breakfast:
+    print("add grits")
+
+else:
+    print("we have no grits")
+    
+if 'cereal' in breakfast:
+    print("add coffee")
+    
+if "home fries" in sides:
+    print("would you like any ketchup?")
+    
+    
+if "coke" not in "drinks":
+    print("can I have tea please? ")
+    
+    
+print("Your order is up")
+print("Thanks for visiting Betty's!")
